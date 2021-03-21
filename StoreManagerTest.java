4ada9f4709314969f97684987c840f6d1a8fd6ba
Keeferf <61815301+Keeferf@users.noreply.github.com>
2021-03-21 14:49:10 -0400

    Update StoreManagerTest.java

--- StoreManagerTest.java
+++ StoreManagerTest.java
@@ -1,10 +1,11 @@
-package Test;//STUDENT NAME: Chia-Yu Liu
+package Test;
+
+//STUDENT NAME: Chia-Yu Liu
 //STUDENT ID: 100698737
 //STUDENT NAME: Keefer Belanger
 //STUDENT ID: St# 101152085
 
 import Store.*;
-import org.junit.Before;
 import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
@@ -14,123 +15,129 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StoreManagerTest {
 
-  private static StoreManager sm;
+    private static StoreManager sm;
 
-  private static Inventory inv1;
-  private static Inventory inv2;
-  private static Inventory inv3;
+    private static Inventory inv1;
+    private static Inventory inv2;
+    private static Inventory inv3;
 
-  private static Product p1;
-  private static Product p2;
-  private static Product p3;
+    private static Product p1;
+    private static Product p2;
+    private static Product p3;
 
-  private static ShoppingCart cart;
+    private static ShoppingCart cart;
 
-  private static int cartID;
+    private static int cartID;
 
+    /**
+     * Sets all products and inventory quantities before running tests
+     */
+    @BeforeAll
+    public static void init(){
 
-  @BeforeAll
-  public static void init(){
+        p1 = new Product("apple", 101, 1.0);
+        p2 = new Product("banana", 202, 2.0);
+        p3 = new Product("iPhone", 303, 3.0);
 
-      p1 = new Product("apple", 101, 1.0);
-      p2 = new Product("banana", 202, 2.0);
-      p3 = new Product("iPhone", 303, 3.0);
+        inv1 = new Inventory(p1, 10);
+        inv2 = new Inventory(p2, 20);
+        inv3 = new Inventory(p3, 30);
 
-      inv1 = new Inventory(p1, 10);
-      inv2 = new Inventory(p2, 20);
-      inv3 = new Inventory(p3, 30);
+        sm = new StoreManager();
 
-      sm = new StoreManager();
-
-      sm.addInventory(inv1);
-
-
-      cartID = sm.assignNewCartID();
-
-      cart = new ShoppingCart();
-
-      cart.addToCart(101, 5);
-
-  }
-
-  @Test //Chia-Yu
-  public void TestCheckStock() {
-      sm.addInventory(inv1);
-      assertEquals(10, sm.checkStock(101),"Either the store manager's  " +
-              "checkStock method is not working or inventory cannot get quantity properly");
-  }
-
-  @Test
-  public void newCart() {
-  }
+        sm.addInventory(inv1);
 
 
-  @Test
-  void addToCart() {
-      ShoppingCart s = new ShoppingCart();
-      s.addToCart(4, 20);
-      assertEquals(20, s.getItems().get(4));
-  }
+        cartID = sm.assignNewCartID();
 
-  @Test //Chia-Yu
-  public void TestRemoveFromCart() {
-      ShoppingCart s = new ShoppingCart();
-      s = cart;
-      s.removeFromCart(101,4);
+        cart = new ShoppingCart();
 
-      assertEquals(1, s.getItems().get(101), "Either the store manager's  " +
-              "removeFromCart method is not working or shopping cart cannot add items properly");
-  }
+        cart.addToCart(101, 5);
 
-  @Test // Chia-Yu
-  public void TestProcessTransaction() {
-      ShoppingCart s = new ShoppingCart();
-      s = cart;
+    }
 
-      assertEquals(5.0, sm.processTransaction(s), "Either the store manager's  " +
-              "processTransaction method is not working or shopping cart cannot iterate cart items properly");
-  }
+    /**
+     * Tests CheckStock by comparing expected value from using the checkStock method against the actual value
+     */
+    @Test
+    public void TestCheckStock() {
+        sm.addInventory(inv1);
+        assertEquals(10, sm.checkStock(101),"Either the store manager's  " +
+                "checkStock method is not working or inventory cannot get quantity properly");
+    }
 
-  @Test // Chia-Yu....half way done
-  public void TestQuit() {
-      ShoppingCart s2 = new ShoppingCart();
-      s2.addToCart(202, 10);
-      sm.quit(s2, cartID);
+    @Test
+    public void newCart() {
+    }
 
-      assertEquals(0, sm.checkStock(202), "Either the store manager's  " +
-              "quit method is not working or shopping cart cannot be removed properly");
-  }
+    /**
+     * Tests addToCart by comparing expected output against actual output
+     */
+    @Test
+    void addToCart() {
+        ShoppingCart s = new ShoppingCart();
+        s.addToCart(4, 20);
+        assertEquals(20, s.getItems().get(4));
+    }
 
-  @Test //Keefer done
-  void getInventories() {
-      ArrayList<Inventory> I = new ArrayList<>();
-      StoreManager m = new StoreManager();
+    /**
+     * Tests removeFromCart by assigning a variable to the cart and removing a specific item from the BeforeAll, and
+     * comparing the expected value from doing this to the actual value from calling the method
+     */
+    @Test
+    public void TestRemoveFromCart() {
+        ShoppingCart s = new ShoppingCart();
+        s = cart;
+        s.removeFromCart(101,4);
 
-      ArrayList<Inventory> expected = new ArrayList<>();
-      assertEquals(expected, m.getInventories());
-  }
+        assertEquals(1, s.getItems().get(101), "Either the store manager's  " +
+                "removeFromCart method is not working or shopping cart cannot add items properly");
+    }
 
-  @Test //Keefer done
-  void assignNewCartID() {
-      StoreManager m = new StoreManager();
+    /**
+     * Tests processTransaction by using the assertEqual to compare expected output to actual output
+     */
+    @Test
+    public void TestProcessTransaction() {
+        ShoppingCart s = new ShoppingCart();
+        s = cart;
 
-      assertEquals(0, m.assignNewCartID());
-  }
+        assertEquals(5.0, sm.processTransaction(s), "Either the store manager's  " +
+                "processTransaction method is not working or shopping cart cannot iterate cart items properly");
+    }
 
-  @Test
-  public void TestAddInventory() {
-      Product p4 = new Product("Product 4", 404, 4.0);
-      Inventory inv4 = new Inventory(p4, 400);
-      sm.addInventory(inv4);
+    /**
+     * Tests quit by adding an item to the cart and quitting out of the store to check if item returns back to stock
+     */
+    @Test
+    public void TestQuit() {
+        ShoppingCart s2 = new ShoppingCart();
+        s2.addToCart(202, 10);
+        sm.quit(s2, cartID);
 
-      assertEquals( sm.getInventories().size(),2 ,"Either the store manager's  " +
-              "addInventory method is not working or the inventory is not created properly");
-  }
+        assertEquals(0, sm.checkStock(202), "Either the store manager's  " +
+                "quit method is not working or shopping cart cannot be removed properly");
+    }
 
-  @Test
-  void findCart() {
-  }
+    /**
+     * Tests getInventory by creating a new inventory and compares it to the expected Inventory
+     */
+    @Test
+    void getInventories() {
+        ArrayList<Inventory> I = new ArrayList<>();
+        StoreManager m = new StoreManager();
 
+        ArrayList<Inventory> expected = new ArrayList<>();
+        assertEquals(expected, m.getInventories());
+    }
 
-    //@Test
+    /**
+     * Tests assignNewCart by assigning a new cart and comparing it to expected of an empty cart
+     */
+    @Test
+    void assignNewCartID() {
+        StoreManager m = new StoreManager();
+
+        assertEquals(0, m.assignNewCartID());
+    }
 }
